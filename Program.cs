2072cfda2019cdace343caad10e24ebfecc2c5f0
smvss14 <80742222+smvss14@users.noreply.github.com>
2024-03-03 20:13:22 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class MyProgram
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                if(i % 3 == 0 && i % 5 == 0) 
+                {
+                    Console.WriteLine("FizzBuzz");
+                    Thread.Sleep(1000);
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                    Thread.Sleep(1000);
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                    Thread.Sleep(1000);
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
